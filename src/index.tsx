4fe336eea878a583526639efc8c27d08834324b3
Victoria Aleksandrova <vicaleksan@gmail.com>
2025-02-13 15:46:52 +0100

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/jsx-indent */
+import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
+import './index.css';
+import 'normalize.css';
+
+function App() {
+    return (
+        <p>All Notes</p>
+    );
+}
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+    createRoot(rootElement).render(
+        <StrictMode>
+            <App />
+        </StrictMode>,
+    );
+} else {
+    // eslint-disable-next-line no-console
+    console.error('root is not found');
+}
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.tsx',
+    mode: 'development',
+    devtool: 'source-map',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    stats: {
+        children: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new MiniCssExtractPlugin(),
+    ],
+    resolve: {
+        modules: [
+            path.resolve(__dirname, 'src'),
+            'node_modules',
+        ],
+        extensions: ['.tsx', '.ts', '.js', '.jsx', '.json'],
+    },
+    devServer: {
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.ttf$/,
+                type: 'asset/resource',
+              },
+            {
+                test: /\.css$/,
+                exclude: /\.module\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.module\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            defaultExport: true,
+                        },
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                localIdentName: '[local]_[hash:base64:3]',
+                            },
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+};
