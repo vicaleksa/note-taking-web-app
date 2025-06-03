1635e136361495e34a7aee0809e7797a485cfdd4
Victoria Aleksandrova <95140115+vicaleksa@users.noreply.github.com>
2025-06-03 17:08:30 +0200

    add note card

---
+++ src/components/NoteCard/index.tsx
@@ -0,0 +1,27 @@
+import styles from './style.module.css';
+
+type NoteCardProps = {
+    title: string,
+    tags: string[],
+    date: string,
+}
+
+export default function NoteCard({
+    title,
+    tags,
+    date,
+}: NoteCardProps) {
+    const tagElements = tags.map((tag) => (
+        <div key={tag} className={styles.tag}>{tag}</div>
+    ));
+
+    return (
+        <div className={styles.card}>
+            <p className={styles.title}>{title}</p>
+            <div className={styles.tagsWrapper}>
+                {tagElements}
+            </div>
+            <p className={styles.date}>{date}</p>
+        </div>
+    );
+}
