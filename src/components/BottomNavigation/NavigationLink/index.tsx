c59eb89f3d7f9f5bcd2043649003598ec3efac88
Victoria Aleksandrova <95140115+vicaleksa@users.noreply.github.com>
2025-06-10 13:35:36 +0200

    add nav link

---
+++ src/components/BottomNavigation/NavigationLink/index.tsx
@@ -0,0 +1,27 @@
+import clsx from 'clsx';
+import { NavLink } from 'react-router';
+import styles from './style.module.css';
+import Icon, { IconType } from '../../Icons/Icon';
+
+type NavigationLinkProps = {
+    link: string;
+    icon: IconType;
+    text: string;
+}
+
+const getNavClassName = ({ isActive }: { isActive: boolean }) => clsx(
+    styles.iconButton,
+    { [styles.iconButtonActive]: isActive },
+);
+
+export default function NavigationLink({ link, icon, text }: NavigationLinkProps) {
+    return (
+        <NavLink
+            to={link}
+            className={getNavClassName}
+        >
+            <Icon type={icon} />
+            <span className={styles.text}>{text}</span>
+        </NavLink>
+    );
+}
--- src/components/BottomNavigation/index.tsx
+++ src/components/BottomNavigation/index.tsx
@@ -1,55 +1,34 @@
-import { NavLink } from 'react-router';
-import clsx from 'clsx';
 import styles from './style.module.css';
-import IconHome from '../../assets/images/IconHome';
-import IconSearch from '../../assets/images/IconSearch';
-import IconArchive from '../../assets/images/IconArchive';
-import IconTag from '../../assets/images/IconTag';
-import IconSettings from '../../assets/images/IconSettings';
-
-const getNavClassName = ({ isActive }: { isActive: boolean }) => clsx(
-    styles.iconButton,
-    { [styles.iconButtonActive]: isActive },
-);
+import NavigationLink from './NavigationLink';
 
 export default function BottomNavigation() {
     return (
         <div className={styles.navbar}>
-            <NavLink
-                to="/"
-                className={getNavClassName}
-            >
-                <IconHome />
-                <span className={styles.text}>Home</span>
-            </NavLink>
-            <NavLink
-                to="/search"
-                className={getNavClassName}
-            >
-                <IconSearch />
-                <span className={styles.text}>Search</span>
-            </NavLink>
-            <NavLink
-                to="/archived"
-                className={getNavClassName}
-            >
-                <IconArchive />
-                <span className={styles.text}>Archived</span>
-            </NavLink>
-            <NavLink
-                to="/tags"
-                className={getNavClassName}
-            >
-                <IconTag />
-                <span className={styles.text}>Tags</span>
-            </NavLink>
-            <NavLink
-                to="/settings"
-                className={getNavClassName}
-            >
-                <IconSettings />
-                <span className={styles.text}>Settings</span>
-            </NavLink>
+            <NavigationLink
+                link="/"
+                icon="home"
+                text="Home"
+            />
+            <NavigationLink
+                link="/search"
+                icon="search"
+                text="Search"
+            />
+            <NavigationLink
+                link="/archived"
+                icon="archive"
+                text="Archived"
+            />
+            <NavigationLink
+                link="/tags"
+                icon="tag"
+                text="Tags"
+            />
+            <NavigationLink
+                link="/settings"
+                icon="settings"
+                text="Settings"
+            />
         </div>
     );
 }
