3b12f62e5b94f8db7063e5467e734532a402d904
Victoria Aleksandrova <95140115+vicaleksa@users.noreply.github.com>
2025-06-20 12:47:44 +0200

    make note cards link

--- src/pages/Notes/index.tsx
+++ src/pages/Notes/index.tsx
@@ -1,4 +1,4 @@
-import { Fragment } from 'react';
+import { Link } from 'react-router';
 import NoteCard from '../../components/NoteCard';
 import styles from './style.module.css';
 import data from '../../data.json';
@@ -6,14 +6,19 @@ import FAB from '../../components/FloatingActionButton';
 
 export default function Notes() {
     const notesElements = data.notes.map((note) => (
-        <Fragment key={note.title}>
-            <NoteCard
-                title={note.title}
-                tags={note.tags}
-                date={note.lastEdited}
-            />
+        <>
+            <Link
+                key={note.id}
+                to={note.id}
+            >
+                <NoteCard
+                    title={note.title}
+                    tags={note.tags}
+                    date={note.lastEdited}
+                />
+            </Link>
             <div className={styles.divider} />
-        </Fragment>
+        </>
     ));
     return (
         <>
