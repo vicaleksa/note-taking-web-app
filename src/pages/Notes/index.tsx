9756d6631ca232bb1a0070ce1ea604c181f13cf1
Victoria Aleksandrova <95140115+vicaleksa@users.noreply.github.com>
2025-06-20 16:11:31 +0200

    add empty notes state

--- src/pages/Notes/index.tsx
+++ src/pages/Notes/index.tsx
@@ -1,16 +1,16 @@
 import { Link } from 'react-router';
+import { Fragment } from 'react/jsx-runtime';
 import NoteCard from '../../components/NoteCard';
 import styles from './style.module.css';
 import data from '../../data.json';
 import FAB from '../../components/FloatingActionButton';
+import Alert from '../../components/Alert';
+import { StoredData } from '../../types';
 
 export default function Notes() {
-    const notesElements = data.notes.map((note) => (
-        <>
-            <Link
-                key={note.id}
-                to={note.id}
-            >
+    const notesElements = (data as StoredData).notes.map((note) => (
+        <Fragment key={note.id}>
+            <Link to={note.id}>
                 <NoteCard
                     title={note.title}
                     tags={note.tags}
@@ -18,12 +18,18 @@ export default function Notes() {
                 />
             </Link>
             <div className={styles.divider} />
-        </>
+        </Fragment>
     ));
     return (
         <>
             <h1 className={styles.title}>All Notes</h1>
-            {notesElements}
+            {notesElements.length > 0
+                ? notesElements
+                : (
+                    <Alert
+                        text="You don’t have any notes yet. Start a new note to capture your thoughts and ideas."
+                    />
+                )}
             <FAB
                 size="small"
                 ariaLabel="Create a new note"
