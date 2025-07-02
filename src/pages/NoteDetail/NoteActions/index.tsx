fef5b644d7a87e41051daf4f45f9867e7ed042d9
Victoria Aleksandrova <95140115+vicaleksa@users.noreply.github.com>
2025-07-02 11:59:44 +0200

    add go back navigation

--- src/pages/NoteDetail/NoteActions/index.tsx
+++ src/pages/NoteDetail/NoteActions/index.tsx
@@ -1,10 +1,19 @@
+import { Link } from 'react-router';
 import Button from '../../../components/Button';
 import styles from './style.module.css';
+import IconArrowLeft from '../../../components/Icons/IconArrowLeft';
 
 export default function NoteActions() {
     return (
         <div className={styles.navContainer}>
-            <Button variant="ghost" buttonText="Go Back" leftIcon="arrowLeft" />
+            <Link
+                to=".."
+                relative="path"
+                className={styles.backButton}
+            >
+                <IconArrowLeft className={styles.backIcon} />
+                Go Back
+            </Link>
             <div className={styles.rightControl}>
                 <Button variant="ghost" leftIcon="delete" />
                 <Button variant="ghost" leftIcon="archive" />
