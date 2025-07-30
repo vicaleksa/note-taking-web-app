11b80df546538ce07c43f4f96b9a7aa9035e4745
Victoria Aleksandrova <95140115+vicaleksa@users.noreply.github.com>
2025-07-30 17:09:16 +0200

    add note list

---
+++ src/components/NoteList/index.tsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router';
+import { Fragment } from 'react/jsx-runtime';
+import NoteCard from '../NoteCard';
+import styles from './style.module.css';
+import { Note } from '../../types';
+
+interface NoteListProps {
+    notes: Note[],
+}
+
+export default function NoteList({ notes }: NoteListProps) {
+    const noteListElement = notes.map((note) => (
+        <Fragment key={note.id}>
+            <Link to={note.id}>
+                <NoteCard
+                    title={note.title || 'Untitled Note'}
+                    tags={note.tags}
+                    date={note.lastEdited}
+                />
+            </Link>
+            <div className={styles.divider} />
+        </Fragment>
+    ));
+
+    return noteListElement;
+}
