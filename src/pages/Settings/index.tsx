22bbcbfa3aacf2d1e569b7bfc32e31e5bed54ad4
Victoria Aleksandrova <95140115+vicaleksa@users.noreply.github.com>
2025-08-06 13:22:22 +0200

    fix menu item links in settings

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -7,23 +7,23 @@ export default function Settings() {
             <h1 className={styles.title}>Settings</h1>
             <div className={styles.menu}>
                 <MenuItem
-                    link="/theme"
+                    link="/settings/theme"
                     leftIcon="sun"
                     text="Color Theme"
                 />
                 <MenuItem
-                    link="/font"
+                    link="/settings/font"
                     leftIcon="font"
                     text="Font Theme"
                 />
                 <MenuItem
-                    link="/password"
+                    link="/settings/password"
                     leftIcon="lock"
                     text="Change Password"
                 />
                 <div className={styles.divider} />
                 <MenuItem
-                    link="/logout"
+                    link="/settings/logout"
                     leftIcon="logout"
                     text="Logout"
                 />
