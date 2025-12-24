c180d4fb4e3f044f28633c360ff46ab3c04c0a78
Victoria Aleksandrova <95140115+vicaleksa@users.noreply.github.com>
2025-12-24 12:36:15 +0100

    add user validation

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,5 +1,22 @@
 import { Request, Response } from 'express';
+import validator from 'validator';
 
 export async function registerUser(req: Request, res: Response) {
-    console.log('req.body: ', req.body);
+    let { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ error: 'All fields are required' });
+    }
+
+    email = email.trim();
+
+    if (!validator.isEmail(email)) {
+        return res.status(400).json({ error: 'Invalid email format' });
+    }
+
+    if (password.length < 6) {
+        return res.status(400).json({ error: 'Password must be at least 6 characters' });
+    }
+
+    console.log(req.body)
 }
